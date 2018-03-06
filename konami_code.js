253207babbd5f5731b493de83d3871b5903b4440
Ryan O'Toole <otoolerj@gmail.com>
2018-03-06 19:08:28 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,7 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
+
+let index = 0;
 function init(e) {
   document.addEventListener('keydown', function(e) {
       console.log(e.which)
@@ -7,7 +9,7 @@ function init(e) {
 
   const key = parseInt(e.detail || e.which);
 
-  let index = 0;
+
 
 
   if (key === code[index]) {
